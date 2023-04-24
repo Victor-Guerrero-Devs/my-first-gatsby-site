e5a840db1f94ddec3e64e1214027e15e1e58444d
Victor Guerrero <vicguerrero.dev@gmail.com>
2023-04-23 22:39:09 -0500

    FInish part 4, SEO & blog files

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import Seo from "../components/seo";
 
 const AboutPage = () => {
   return (
@@ -11,10 +12,6 @@ const AboutPage = () => {
   );
 };
 
-export const Head = () => (
-  <>
-    <title>About Me</title>
-    <meta name="description" content="my first gatsby site" />
-  </>
-);
+export const Head = () => <Seo title="About Page" />;
+
 export default AboutPage;
